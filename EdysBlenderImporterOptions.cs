97a6d483ddae541b44540b275f6a9cb7e56b2066
frdfsnlght <frdfsnlght@gmail.com>
2014-07-30 16:13:16 -0400

    Added custom asset for options.

---
+++ EdysBlenderImporterOptions.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class EdysBlenderImporterOptions : ScriptableObject
+    {
+
+    public bool fixBlender = true;
+    public bool optimize = false;
+    public bool zReverse = false;
+    public bool animFix = true;
+    public bool floatFix = true;
+    public bool postMods = true;
+    public bool forceFixRoot = false;
+    
+    public float floatFixThreshold = 1.53e-05f;
+
+    public override string ToString()
+        {
+        return
+            (fixBlender ? "fixBlender " : "") +
+                (optimize ? "optimize " : "") +
+                (zReverse ? "zReverse " : "") +
+                (animFix ? "animFix " : "") +
+                (floatFix ? "floatFix " : "") +
+                (postMods ? "postMods " : "") +
+                (forceFixRoot ? "forceFixRoot " : "");
+        }
+
+#if UNITY_EDITOR
+
+    [UnityEditor.MenuItem ("Assets/Create/EdysBlenderImporter Options", false, 9999)]
+    static void CreateAsset ()
+        {
+        EdysBlenderImporterOptions asset = ScriptableObject.CreateInstance<EdysBlenderImporterOptions>();
+        UnityEditor.AssetDatabase.CreateAsset(asset, "Assets/New EdysBlenderImporterOptions.asset");
+        UnityEditor.AssetDatabase.SaveAssets();
+        UnityEditor.EditorUtility.FocusProjectWindow();
+        UnityEditor.Selection.activeObject = asset;
+        }
+
+#endif
+
+    }
